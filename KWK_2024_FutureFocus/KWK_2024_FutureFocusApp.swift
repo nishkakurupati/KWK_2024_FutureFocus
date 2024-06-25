80ef6058960a873e1ab2a536da79a34f251441da
nishkakurupati <nishkakurupati@gmail.com>
2024-06-25 13:01:50 -0700

    Initial Commit

---
+++ KWK_2024_FutureFocus/KWK_2024_FutureFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWK_2024_FutureFocusApp.swift
+//  KWK_2024_FutureFocus
+//
+//  Created by Scholar on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct KWK_2024_FutureFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
